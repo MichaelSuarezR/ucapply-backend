df6f6c20d441d468b9f93969f38deb962b4eb42e
MichaelSuarezR <96852398+MichaelSuarezR@users.noreply.github.com>
2025-06-06 15:35:58 -0700

    Update config.js

--- internship_api/config.js
+++ internship_api/config.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
-  APP_ID: process.env.APP_ID,
-  API_KEY: process.env.API_KEY,
+  APP_ID: process.env.ADZUNA_APP_ID,
+  API_KEY: process.env.ADZUNA_APP_KEY,
   BASE_URL: 'https://api.adzuna.com/v1/api/jobs',
-  BASE_PARAMS: 'search/1?&results_per_page=20&content-type=application/json',
+  BASE_PARAMS: 'search/1?results_per_page=20&content-type=application/json',
 };
